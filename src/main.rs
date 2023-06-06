715db7a3c9028c656bc456a8387b6af041e6879c
Veljko Markovic <veljkomarkovic@Veljkos-Air.lan>
2023-06-06 15:54:49 +0200

    boje :DD

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 use std::fs;
 use std::path::Path;
+const COLOR_RESET: &str = "\x1B[0m";
+const COLOR_FOLDER: &str = "\x1B[1;34m";//Zelena DDDclea
+const COLOR_FILE: &str = "\x1B[0;32m";//plava :DDD
 fn main() {
     let curr_dir=env::current_dir().unwrap();
     tree(&curr_dir,0);
@@ -14,13 +17,21 @@ fn tree(path:&Path,level:usize) {
         let entry_name= entry.file_name();
         let file_name= entry_name.to_string_lossy();
         
-        print!("{}", "  ".repeat(level));
-        
-        println!("{}", file_name);
+        print!("|");
+        print!("{}", "-".repeat(level));
         
         if entry.file_type().unwrap().is_dir() {
+            
+            println!("{}{}{}", COLOR_FOLDER, file_name, COLOR_RESET);
+            
             let sub_path = path.join(entry_name);
             tree(&sub_path, level + 1);
+        } else {
+            println!("{}{}{}", COLOR_FILE, file_name, COLOR_RESET);
         }
     }
 }
+//TODO
+//ignorisanje fajlova/foldera, vrv minimalna hesh tablica
+//da bude malo vise "rust idiomatic"
+//permission denied ako sam u 
