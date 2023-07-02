8d1c232e1de35430b13e1dc8a5f92e1c0ba663b0
DrainGangScholar <veljko.markovic@elfak.rs>
2023-07-02 14:16:22 +0200

    wowwww

--- tree.cpp
+++ tree.cpp
@@ -8,13 +8,20 @@ using std::string;
 using std::vector;
 using std::sort;
 
-void tree(string directory);
+string inner[2]={"├── ", "│   "};
+string final[2]={ "└── ", "    "};
+
+
+size_t files=0;
+size_t folders=0;
+
+void tree(string directory,string prefix);
 
 int  main(int argc, char*argv[]) {
     //current dir
     //tree()
     // string root= argc==1? ".":argv[1];
-    tree(fs::current_path().string());
+    tree(fs::current_path().string(),"");
     return 0;
 }
 
@@ -23,17 +30,34 @@ auto compare=[](const fs::directory_entry &left,const fs::directory_entry &right
     return left.path().filename()<right.path().filename();
 };
 
-void tree(string directory){
+void tree(string directory,string prefix){
     vector<fs::directory_entry> entries;
 
     for(const auto &entry: fs::directory_iterator(directory)) {
-        if(entry.path().filename().string()[0]!='.')
+        if(entry.path().filename().string()[0]!='.'){
             entries.emplace_back(entry);
+        }
     }
-    
+
     sort(entries.begin(), entries.end(),compare);
 
+    int index=0;
     for(const auto &entry:entries){
-        cout<<entry.path().filename().string()<<endl;
+        string pointers[2];
+        if(index==entries.size()-1){
+            pointers[0]=final[0];
+            pointers[1]=final[1];
+        }else{
+            pointers[0]=inner[0];
+            pointers[1]=inner[1];
+        }
+        cout<<prefix<<pointers[0]<<entry.path().filename().string()<<endl;
+        index++;
+        if(entry.is_regular_file()){
+            files++;
+        }else {
+            folders++;
+            tree(entry.path().string(),pointers[1]);
+        }
     }
 }
